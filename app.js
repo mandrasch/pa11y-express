702a79bcdf1804ded51b6ad66c5e435304a8b892
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-05-13 14:17:39 +0200

    revamp api to use GET, it's easier

--- app.js
+++ app.js
@@ -7,35 +7,49 @@ const bodyParser = require('body-parser');
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', async function (req, res) {
+/**
+ * GET /check endpoint
+ */
+app.get("/check", async function (req, res) {
 
-  console.log('serverMiddleware: Launching pa11y check ...', req.query, req.body);
+  console.log('GET /check ...');
 
-  if (!req.body.url) {
-    res.status(500).send('No URL submitted');
+  // Check if url is submitted
+  if (!req.query.url) {
+    res.status(500).send({ error: 'No URL submitted' });
     return;
   }
-  // TODO: check if url was given
-  // TODO: sanitize?
-  const resultJson = await pa11y(req.body.url, {
+
+  console.log('Fire up pa11y ...');
+  const resultJson = await pa11y(req.query.url, {
     reporter: 'json',
   });
-
-  console.log('serverMiddleware: Returning pa11y results ...', resultJson);
-
+  // TODO: sanitize url?
+  // console.log('Returning pa11y results ...', resultJson);
   res.send(resultJson);
-
 })
 
+/**
+ * GET / index page (static HTML)
+ */
 app.get("/", (req, res) => res.type('html').send(html));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`pa11y-express listening on port ${port}!`));
 
+/**
+ * Current url
+ */
+// https://render.com/docs/environment-variables
+const appUrl = process.env.RENDER_EXTERNAL_URL || '';
+
+/**
+ * index page HTML
+ */
 const html = `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hello from Render!</title>
+    <title>Hello from pa11y API endpoint!</title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <script>
       setTimeout(() => {
@@ -69,7 +83,11 @@ const html = `
   </head>
   <body>
     <section>
-      Hello from Render!
+      <h1>Hello from pa11y API endpoint!</h1>
+
+      <p>Use API via GET: <pre>${appUrl}/check?url=https%3A%2F%2Fwww.a11yproject.com%2F</pre></p>
+
+      <p>Source: <a href="https://github.com/mandrasch/pa11y-express">https://github.com/mandrasch/pa11y-express</a></p>
     </section>
   </body>
 </html>
