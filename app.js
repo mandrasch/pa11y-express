9ec8749b8d53d2df998d45e00c41b218c60f73ed
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-05-13 13:47:08 +0200

    re-add sample index page from example

--- app.js
+++ app.js
@@ -26,4 +26,50 @@ app.post('/', async function (req, res) {
 
 })
 
+app.get("/", (req, res) => res.type('html').send(html));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Hello from Render!
+    </section>
+  </body>
+</html>
+`
